672fcc9a6c1291ee1ee3e545328303e47c4c8c42
Manit Arora <71984659+LIGHTUNCHARGED@users.noreply.github.com>
2026-07-29 16:43:02 +0530

    Add Twin Permutations [A] from Codeforces

---
+++ 1831/A - Twin Permutations/solution.cpp
@@ -0,0 +1,22 @@
+//  A. Twin Permutations
+ 
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int t; cin>>t;
+    while (t--) {
+        int n; cin>>n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin>>v[i];
+        }
+        int ref = n+1;
+        for (int i = 0; i < n; i++) {
+            cout<<ref - v[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
